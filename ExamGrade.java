ed477574559d0497ddc707b818c5c65f6b78e212
aadel89 <rohradel@gmail.com>
2022-12-18 12:18:50 +0100

    Update ExamGrade.java

--- ExamGrade.java
+++ ExamGrade.java
@@ -1,54 +1,54 @@
-public class ExamCredit {
+public class ExamGrade {
 
     public static void main(String[] args) {
 
         // equivalence partitioning
 
             // valid equivalence partitions
-            examCredit(12);
-            examCredit(55);
-            examCredit(73);
-            examCredit(84);
-            examCredit(97);
+            examGrade(12);
+            examGrade(55);
+            examGrade(73);
+            examGrade(84);
+            examGrade(97);
 
             //invalid equivalence partitions
-            examCredit(-2);
-            examCredit(102);
+            examGrade(-2);
+            examGrade(102);
             //other options with black box technique: letters, special characters
             
         //2 points boundary value analyses
-            examCredit(-1);
-            examCredit(0);
-            examCredit(54);
+            examGrade(-1);
+            examGrade(0);
+            examGrade(54);
 
-            examCredit(55);
-            examCredit(64);
+            examGrade(55);
+            examGrade(64);
 
-            examCredit(65);
-            examCredit(74);
+            examGrade(65);
+            examGrade(74);
 
-            examCredit(75);
-            examCredit(84);
+            examGrade(75);
+            examGrade(84);
 
-            examCredit(85);
-            examCredit(100);
+            examGrade(85);
+            examGrade(100);
 
-            examCredit(101);
+            examGrade(101);
 
     }
 
-    public static void examCredit(int score) {
+    public static void examGrade(int score) {
 
         if (score >= 0 && score < 55) {
             System.out.println("Your final score: " + score + ". You failed the exam.");
         } else if ( score >= 55 && score < 65) {
-            System.out.println("Your final score: " + score + ". You passed the exam. You credit is 2.");
+            System.out.println("Your final score: " + score + ". You passed the exam. Your garde is 2.");
         } else if (score >= 65 && score < 75) {
-            System.out.println("Your final score: " + score + ". You passed the exam. You credit is 3.");
+            System.out.println("Your final score: " + score + ". You passed the exam. Your garde is 3.");
         } else if (score >= 75 && score < 85) {
-            System.out.println("Your final score: " + score + ". You passed the exam. You credit is 4.");
+            System.out.println("Your final score: " + score + ". You passed the exam. Your garde is 4.");
         } else if (score >= 85 && score <= 100) {
-            System.out.println("Your final score: " + score + ". You passed the exam. You credit is 5.");
+            System.out.println("Your final score: " + score + ". You passed the exam. Your garde is 5.");
         } else {
             System.out.println("Invalid value");
         }
